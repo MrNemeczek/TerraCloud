dffaaf7cbfc2e6c7cdef78382b23c8d653f776be
MrNemeczek <jeremiaszkubryn@gmail.com>
2024-03-13 19:32:46 +0100

    initial registration UI added

--- TerraCloud/TerraCloud.Client/Pages/Auth/Registry.razor.cs
+++ TerraCloud/TerraCloud.Client/Pages/Auth/Registry.razor.cs
@@ -1,8 +1,35 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Radzen;
 
 namespace TerraCloud.Client.Pages.Auth
 {
     public class RegistryBase : ComponentBase
     {
+        protected string userName = "admin";
+        protected string password = "admin";
+        protected string email = "admin@wp.pl";
+
+        protected bool popup;
+
+        protected bool OnLogin(LoginArgs args, string name)
+        {
+            Console.WriteLine($"{name} -> Username: {args.Username}, password: {args.Password}, remember me: {args.RememberMe}");
+
+            return true;
+        }
+
+        protected bool OnRegister(string name)
+        {
+            Console.WriteLine($"{name} -> Register");
+
+            return true;
+        }
+
+        protected bool OnResetPassword(string value, string name)
+        {
+            Console.WriteLine($"{name} -> ResetPassword for user: {value}");
+
+            return true;
+        }
     }
 }
