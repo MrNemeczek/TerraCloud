8540337444751b26ba5a84e0736375bfabc7a90c
MrNemeczek <jeremiaszkubryn@gmail.com>
2024-07-22 20:35:08 +0200

    animal details core FE

--- TerraCloud/TerraCloud.Client/Pages/Animal/Details.razor.cs
+++ TerraCloud/TerraCloud.Client/Pages/Animal/Details.razor.cs
@@ -1,8 +1,24 @@
 ﻿using Microsoft.AspNetCore.Components;
 
+using TerraCloud.Application.DTOs.Animal.Responses;
+using TerraCloud.Client.Common;
+
 namespace TerraCloud.Client.Pages.Animal
 {
     public class DetailsBase : ComponentBase
     {
+        [Parameter]
+        public string Id { get; set; } = null!;
+        [Inject]
+        private IApiRequest _apiRequest { get; set; } = default!;
+
+        protected GetAnimalResponse animal = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            animal = await _apiRequest.GetAsync<GetAnimalResponse>($"Animal/{Id}");
+        }
     }
 }
