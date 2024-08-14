f45297a8d1a2d2bbbb594f599aeedcbfdf46df65
MrNemeczek <jeremiaszkubryn@gmail.com>
2024-08-14 21:52:28 +0200

    delete animal if user is not owner

--- TerraCloud.Application/Animal/Commands/DeleteUserAnimal.cs
+++ TerraCloud.Application/Animal/Commands/DeleteUserAnimal.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TerraCloud.Application.Exceptions.Animal;
+﻿using TerraCloud.Application.Exceptions.Animal;
 using TerraCloud.Persistence.Interfaces.Repository.Animal;
 
 namespace TerraCloud.Application.Animal.Commands
